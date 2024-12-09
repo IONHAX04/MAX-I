e91fcd44be07f7e611720873e48974ec14b53fdf
IONHAX04 <thirukumara04@gmail.com>
2024-12-09 09:18:27 +0530

    Contact us page

--- src/components/05-Contact/Contact.jsx
+++ src/components/05-Contact/Contact.jsx
@@ -1,193 +1,191 @@
-import { motion } from "framer-motion";
-import { twMerge } from "tailwind-merge";
-import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
-import { SiGithub, SiTiktok, SiYoutube } from "react-icons/si";
-import { FaXTwitter } from "react-icons/fa6";
-
-import PropTypes from "prop-types";
-
 import contact from "../../assets/common/contact.jpg";
+import { useNavigate } from "react-router-dom";
+
+import "./Contact.css";
 
 export default function Contact() {
+  const navigate = useNavigate();
+
   return (
     <>
       <img src={contact} alt="" />
-      <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-        <motion.div
-          initial="initial"
-          animate="animate"
-          transition={{
-            staggerChildren: 0.05,
-          }}
-          className="mx-auto grid max-w-2xl grid-flow-dense grid-cols-12 gap-4"
-        >
-          <HeaderBlock />
-          <SocialsBlock />
-          <AboutBlock />
-          <LocationBlock />
-          <EmailListBlock />
-        </motion.div>
+      <div className="bg-white">
+        <div className="w-[100%]" align="center">
+          <h1
+            className=" w-[90%] lg:-[100%] text-[35px] font-bold capitalize text-[#000] py-10"
+            data-aos="fade-up"
+            align="center"
+          >
+            Get In Touch
+          </h1>
+
+          <div className="w-[90%] lg:w-[75%] bg-[#F5F7F8] flex pb-10 flex-col lg:flex-row">
+            <div
+              className="w-[100%] lg:w-[40%] bg-[#fff] rounded-l-md"
+              style={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;",
+              }}
+            >
+              <div className="pt-10">
+                <a
+                  target="_blank"
+                  href="tel:+41763953921"
+                  className="w-[90%] cursor-pointer"
+                >
+                  <div className="text-[15px]">
+                    <i className="fa-solid fa-phone border border-[#ffaa14] p-3 text-[#ffaa14] rounded-[50%]"></i>
+                  </div>
+                  <div className="mt-3 text-[18px] text-[#000] font-bold font-sans">
+                    +41 76 395 39 21
+                  </div>
+                </a>
+              </div>
+
+              <div className="pt-8">
+                <a
+                  target="_blank"
+                  href="mailto:contact@max-idigital.com"
+                  className="w-[90%] cursor-pointer"
+                >
+                  <div className="text-[15px]">
+                    <i className="fa-regular fa-envelope border border-[#ffaa14] p-3 text-[#ffaa14] rounded-[50%]"></i>
+                  </div>
+                  <div className="mt-3 text-[18px] text-[#000] font-bold font-sans">
+                    contact-us@max-idigital.com
+                  </div>
+                </a>
+              </div>
+
+              <div className="pt-8">
+                <a
+                  target="_blank"
+                  href="https://maps.app.goo.gl/xzAdmKzvMFFXgoqB7"
+                  className="w-[70%] cursor-pointer"
+                >
+                  <div className="text-[15px]">
+                    <i className="fa-solid fa-location-dot border border-[#ffaa14] p-3 px-4 text-[#ffaa14] rounded-[50%]"></i>
+                  </div>
+                  <div className="mt-3 text-[18px] text-[#000] font-bold font-sans">
+                    {/* 38/37 No.1 Logi Street, Gugai,
+                <br /> Salem - 636006, Tamilnadu,
+                <br /> India */}
+                    Corporate Office: <br /> Güterstrasse 8, 8952 Schlieren,
+                    <br /> Switzerland
+                  </div>
+                </a>
+              </div>
+
+              <div className="pt-8 pb-10">
+                <a
+                  target="_blank"
+                  href="https://maps.app.goo.gl/jXgitCchAK13imy36"
+                  className="w-[70%] cursor-pointer"
+                >
+                  <div className="text-[15px]">
+                    <i className="fa-solid fa-location-dot border border-[#ffaa14] p-3 px-4 text-[#ffaa14] rounded-[50%]"></i>
+                  </div>
+                  <div className="mt-3 text-[18px] text-[#000] font-bold font-sans">
+                    Branch Office: <br /> 38/37B, No.1 Logi Street,
+                    <br /> Gugai, Salem - 636006,
+                    <br /> Tamilnadu, India
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="w-[100%] bg-[#fff] rounded-r-md lg:w-[60%]">
+              <div className="w-[90%]">
+                <div className="text-[30px] font-bold capitalize text-[#000] pt-10 lg:pt-20 pb-5">
+                  <span className="text-[#ffaa14] uppercase underline">
+                    drop us a line
+                  </span>
+                </div>
+
+                <div className="relative my-6">
+                  <input
+                    id="id-l01"
+                    type="text"
+                    name="id-l01"
+                    placeholder="your name"
+                    className="relative w-full h-12 px-4 placeholder-transparent transition-all border rounded outline-none focus-visible:outline-none peer bg-[#fff] border-slate-200 text-slate-500 autofill:bg-[#fff] invalid:border-pink-500 invalid:text-pink-500 focus:border-[#ffaa14] focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                  />
+                  <label
+                    for="id-l01"
+                    className="cursor-text peer-focus:cursor-default peer-autofill:-top-2 absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-[#fff] before:transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-[#ffaa14] peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                  >
+                    Enter Your Name
+                  </label>
+                </div>
+
+                <div className="relative my-6">
+                  <input
+                    id="id-l02"
+                    type="text"
+                    name="id-l01"
+                    placeholder="your name"
+                    className="relative w-full h-12 px-4 placeholder-transparent transition-all border rounded outline-none focus-visible:outline-none peer bg-[#fff] border-slate-200 text-slate-500 autofill:bg-[#fff] invalid:border-pink-500 invalid:text-pink-500 focus:border-[#ffaa14] focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                  />
+                  <label
+                    for="id-l02"
+                    className="cursor-text peer-focus:cursor-default peer-autofill:-top-2 absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-[#fff] before:transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-[#ffaa14] peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                  >
+                    Enter E-Mail
+                  </label>
+                </div>
+
+                <div className="relative  pb-5">
+                  <textarea
+                    id="id-l03"
+                    type="text"
+                    name="id-l01"
+                    rows="3"
+                    placeholder="Write your message"
+                    className="relative w-full p-4 placeholder-transparent transition-all border rounded outline-none focus-visible:outline-none peer bg-[#fff] border-slate-200 text-slate-500 autofill:bg-[#ffaa14] invalid:border-pink-500 invalid:text-pink-500 focus:border-[#ffaa14] focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                  ></textarea>
+                  <label
+                    for="id-l03"
+                    className="cursor-text peer-focus:cursor-default absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-[#fff] before:transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-[#ffaa14] peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                  >
+                    Write your message
+                  </label>
+                </div>
+
+                <button className="conatctbutton mb-10 lg:mb-0">
+                  <div className="svg-wrapper-1">
+                    <div className="svg-wrapper">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        height="24"
+                      >
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path
+                          fill="currentColor"
+                          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <span>Contact Us</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-[90%] lg:w-[75%] h-[450px] bg-[#F5F7F8] py-5 lg:py-10">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2700.675779286769!2d8.445608535601265!3d47.3987585371076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900c715bc72bcd%3A0xae40b37a12d40206!2sG%C3%BCterstrasse%208%2C%208952%20Schlieren%2C%20Switzerland!5e0!3m2!1sen!2sin!4v1728300704635!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              style={{ border: "0", borderRadius: "5px" }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
       </div>
     </>
   );
 }
-
-const Block = ({ className, ...rest }) => {
-  return (
-    <motion.div
-      variants={{
-        initial: {
-          scale: 0.5,
-          y: 50,
-          opacity: 0,
-        },
-        animate: {
-          scale: 1,
-          y: 0,
-          opacity: 1,
-        },
-      }}
-      transition={{
-        type: "spring",
-        mass: 3,
-        stiffness: 400,
-        damping: 50,
-      }}
-      className={twMerge(
-        "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
-        className
-      )}
-      {...rest}
-    />
-  );
-};
-
-const HeaderBlock = () => (
-  <Block className="col-span-12 row-span-2 md:col-span-6">
-    <img
-      src="https://api.dicebear.com/8.x/lorelei-neutral/svg?seed=John"
-      alt="avatar"
-      className="mb-4 size-14 rounded-full"
-    />
-    <h1 className="mb-12 text-4xl font-medium leading-tight">
-      Hi, I'm Tom.{" "}
-      <span className="text-zinc-400">
-        I build cool websites like this one.
-      </span>
-    </h1>
-    <a
-      href="#"
-      className="flex items-center gap-1 text-red-300 hover:underline"
-    >
-      Contact me <FiArrowRight />
-    </a>
-  </Block>
-);
-
-const SocialsBlock = () => (
-  <>
-    <Block
-      whileHover={{
-        rotate: "2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-red-500 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        <SiYoutube />
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-green-600 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        <SiGithub />
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-zinc-50 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-black"
-      >
-        <SiTiktok />
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-blue-500 md:col-span-3"
-    >
-      <a
-        href="#"
-        className="grid h-full place-content-center text-3xl text-white"
-      >
-        <FaXTwitter />
-      </a>
-    </Block>
-  </>
-);
-
-const AboutBlock = () => (
-  <Block className="col-span-12 text-3xl leading-snug">
-    <p>
-      My passion is building cool stuff.{" "}
-      <span className="text-zinc-400">
-        I build primarily with React, Tailwind CSS, and Framer Motion. I love
-        this stack so much that I even built a website about it. I've made over
-        a hundred videos on the subject across YouTube and TikTok.
-      </span>
-    </p>
-  </Block>
-);
-
-const LocationBlock = () => (
-  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
-    <FiMapPin className="text-3xl" />
-    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
-  </Block>
-);
-
-const EmailListBlock = () => (
-  <Block className="col-span-12 md:col-span-9">
-    <p className="mb-3 text-lg">Join my mailing list</p>
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="flex items-center gap-2"
-    >
-      <input
-        type="email"
-        placeholder="Enter your email"
-        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
-      />
-      <button
-        type="submit"
-        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
-      >
-        <FiMail /> Join the list
-      </button>
-    </form>
-  </Block>
-);
-
-Block.propTypes = {
-  className: PropTypes.string, // className should be a string
-  children: PropTypes.node.isRequired, // children should be a React node and is required
-};
